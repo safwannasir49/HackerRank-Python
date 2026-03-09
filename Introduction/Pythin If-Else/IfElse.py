e29beb5a2388ac2f623dccefcb51660f03f4a7f2
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2026-03-09 10:58:51 +0530

    Update IfElse.py

--- Introduction/Pythin If-Else/IfElse.py
+++ Introduction/Pythin If-Else/IfElse.py
@@ -10,12 +10,14 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    if n%2==1:
-        print("Weird")
-    elif 2 <= n <=5:
+
+if n%2==0:
+    if n>=2 and n<=5:
         print("Not Weird")
-    elif 6<= n <=20:
+    elif n>=6 and n<=20:
         print("Weird")
     else:
         print("Not Weird")
-         
+else:
+    print("Weird")
+    
